a841fa011f4e313c2c4efdafd1bbebbdc14850f2
paras <parasverma2531@gmail.com>
2022-05-19 18:05:38 +0530

    modified

--- commandLineArgument.java
+++ commandLineArgument.java
@@ -1,16 +1,22 @@
 public class commandLineArgument {
     public static void main(String[] args) {
         // command Line Arguments
-        if(args.length!=0)
+//        if(args.length!=0)
+//        {
+//            for(String str: args)
+//            {
+//                System.out.println(str);
+//            }
+//        }
+//        else
+//        {
+//            System.out.println("No command Line argument passed");
+//        }
+        String [] argsh={"A","B","C"};
+        args=argsh;// string array object assignment
+        for(String s: args)
         {
-            for(String str: args)
-            {
-                System.out.println(str);
-            }
-        }
-        else
-        {
-            System.out.println("No command Line argument passed");
+            System.out.println(s);
         }
     }
 }
