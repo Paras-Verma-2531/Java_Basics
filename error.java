ff4eb0736b20c21a07108d51da28da0755b2cf7e
paras <parasverma2531@gmail.com>
2022-05-21 01:44:57 +0530

    comments modified

--- error.java
+++ error.java
@@ -3,7 +3,7 @@ class error
 public static void main(String[] args)
 {
  public int x=3;
-//local variable cannot have other modifier other than default or final
+//local variable cannot have other modifier other than  final
 System.out.println(x);
 }
 }
