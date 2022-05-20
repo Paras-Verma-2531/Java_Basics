2e082c9fbdd5f9041957918f8a5110709a3d0b8e
paras <parasverma2531@gmail.com>
2022-05-20 22:47:32 +0530

    final class (cannot be inherited)

---
+++ final_class.java
@@ -0,0 +1,14 @@
+final class parent
+{
+public void feature1()
+{
+System.out.println("feature 1");
+}
+}
+class child extends parent
+{
+public void feature2()
+{
+System.out.println("feature 2");
+}
+}
