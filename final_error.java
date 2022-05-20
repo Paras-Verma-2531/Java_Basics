02f45760b8d4b337d9e956d1093572730e329b4c
paras <parasverma2531@gmail.com>
2022-05-20 22:36:46 +0530

    final method overridden error

---
+++ final_error.java
@@ -0,0 +1,15 @@
+class parent
+{
+public final void property()
+{
+System.out.println("cash.......");
+}
+}
+class child extends parent
+{
+public void property()
+{
+
+System.out.println("cash + land......");
+}
+}
