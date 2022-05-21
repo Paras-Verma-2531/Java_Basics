896294cb3e7944673c55a8b610e7b02e6826eff3
paras <parasverma2531@gmail.com>
2022-05-22 01:05:57 +0530

    ServiceProvider main

---
+++ Interfaces_prac/ServiceProviderMain.java
@@ -0,0 +1,8 @@
+package Interfaces_prac;
+public class ServiceProviderMain {
+    public static void main(String[] args) {
+        ServiceProvider sp = new ServiceProvider();
+        sp.feature1();
+        sp.feature2();
+    }
+}
