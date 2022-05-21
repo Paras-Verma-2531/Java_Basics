1451d3c0a0c1db44687077f544353f6a588111f9
paras <parasverma2531@gmail.com>
2022-05-22 01:57:20 +0530

    Interface variables

---
+++ Interfaces_prac/Interf2.java
@@ -0,0 +1,11 @@
+package Interfaces_prac;
+
+public interface Interf2 {
+    /*
+    interfaces can contain variables as well which are public static final by default
+     */
+    String name= " yz";
+    int age=19;
+    void m1();
+    void m2();
+}
