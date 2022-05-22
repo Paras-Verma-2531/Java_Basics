9bccfc0893b27df470c810168738eb98d384c418
paras <parasverma2531@gmail.com>
2022-05-22 18:54:13 +0530

    example

--- Interfaces_prac/Marker_Interface/Intro_class.java
+++ Interfaces_prac/Marker_Interface/Intro_class.java
@@ -3,5 +3,6 @@ package Interfaces_prac.Marker_Interface;
 public class Intro_class {
     public static void main(String[] args) {
         System.out.println("if an interface does not contain any methods and by implementing this interfaces if an objects get some ability then it is said to be marker interface");
+    // if one class's object want to attain serializable property it must impl. serializable interface(Marker Interface)
     }
 }
