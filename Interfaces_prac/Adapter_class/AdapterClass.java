992dcf5ae21e175433ba133a06caa99b7fb10b8a
paras <parasverma2531@gmail.com>
2022-05-22 23:02:19 +0530

    Adapter Class

---
+++ Interfaces_prac/Adapter_class/AdapterClass.java
@@ -0,0 +1,13 @@
+package Interfaces_prac.Adapter_class;
+
+public class AdapterClass implements Interface1{
+    // Adapter Class is a Class which contains all the methods of interface with only empty body
+    public void feature1(){}
+    public void feature2(){}
+    public void feature3(){}
+    public void feature4(){}
+    public void feature5(){}
+    public void feature6(){}
+    public void feature7(){}
+
+}
