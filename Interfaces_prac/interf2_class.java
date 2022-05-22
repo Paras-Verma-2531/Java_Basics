b316043edeea224d597ce20c4795a9d345131c30
paras <parasverma2531@gmail.com>
2022-05-22 17:33:58 +0530

    final interface variable

--- Interfaces_prac/interf2_class.java
+++ Interfaces_prac/interf2_class.java
@@ -1,7 +1,14 @@
 package Interfaces_prac;
 
 public class interf2_class implements  Interf2{
-    //age=23; cannot change the final variable
+
+    public static void main(String[] args) {
+        //age=23; cannot change the final variable
+        System.out.println(age);
+        interf2_class ir =new interf2_class();
+        ir.m1();
+        ir.m2();
+    }
     public void m1()
     {
         System.out.println("hello");
