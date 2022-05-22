669b740aa37a62353f76169fabdc6266805c51fc
paras <parasverma2531@gmail.com>
2022-05-22 22:59:37 +0530

    interface added

---
+++ Interfaces_prac/Adapter_class/Interface1.java
@@ -0,0 +1,12 @@
+package Interfaces_prac.Adapter_class;
+public interface Interface1 {
+    // interface with 7 methods
+    void feature1();
+    void feature2();
+    void feature3();
+    void feature4();
+    void feature5();
+    void feature6();
+    void feature7();
+
+}
