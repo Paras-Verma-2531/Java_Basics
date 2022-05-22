ceef698319df208e8e92a2c515b9b28c7ba49eeb
paras <parasverma2531@gmail.com>
2022-05-22 17:28:46 +0530

    final variable cannot be changed

---
+++ Interfaces_prac/interf2_class.java
@@ -0,0 +1,13 @@
+package Interfaces_prac;
+
+public class interf2_class implements  Interf2{
+    //age=23; cannot change the final variable
+    public void m1()
+    {
+        System.out.println("hello");
+    }
+    public void m2()
+    {
+        System.out.println("hiii");
+    }
+}
