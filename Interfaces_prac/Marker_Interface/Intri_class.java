67ab2189eb6b691effbaff70020ccd222b89009d
paras <parasverma2531@gmail.com>
2022-05-22 18:50:14 +0530

    Marker Interface definition

---
+++ Interfaces_prac/Marker_Interface/Intri_class.java
@@ -0,0 +1,7 @@
+package Interfaces_prac.Marker_Interface;
+
+public class Intri_class {
+    public static void main(String[] args) {
+        System.out.println("if an interface does not contain any methods and by implementing this interfaces if an objects get some ability then it is said to be marker interface");
+    }
+}
