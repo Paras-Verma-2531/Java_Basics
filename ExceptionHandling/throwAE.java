8f062fc48cd5bdfa75076bfa812cf4787ddfc78e
paras <parasverma2531@gmail.com>
2022-05-24 02:30:46 +0530

    reference throws null pointer Exception

--- ExceptionHandling/throwAE.java
+++ ExceptionHandling/throwAE.java
@@ -1,11 +1,11 @@
 package ExceptionHandling;
 
 public class throwAE {
-    static java.lang.ArithmeticException e= new java.lang.ArithmeticException();// throws AE since object is created
-    //static java.lang.ArithmeticException e1;
+    //static java.lang.ArithmeticException e= new java.lang.ArithmeticException();// throws AE since object is created
+    static java.lang.ArithmeticException e1;// throws null pointer Exception
     public static void main(String[] args) {
-        throw e;
-        //throw e1;
+        //throw e;
+        throw e1;
 
     }
 }
