72f7211ccbb509e464a929da7ca0022d060c2b23
paras <parasverma2531@gmail.com>
2022-05-24 02:29:08 +0530

    Arithmetic Exception object

---
+++ ExceptionHandling/throwAE.java
@@ -0,0 +1,11 @@
+package ExceptionHandling;
+
+public class throwAE {
+    static java.lang.ArithmeticException e= new java.lang.ArithmeticException();// throws AE since object is created
+    //static java.lang.ArithmeticException e1;
+    public static void main(String[] args) {
+        throw e;
+        //throw e1;
+
+    }
+}
