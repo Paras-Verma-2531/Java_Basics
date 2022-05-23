04f6cd53c9dd774836ffe7ae19fcd5135c547795
paras <parasverma2531@gmail.com>
2022-05-24 02:42:48 +0530

    cannot throw normal java class objects

---
+++ ExceptionHandling/throwNormalObject.java
@@ -0,0 +1,8 @@
+package ExceptionHandling;
+
+public class throwNormalObject {
+    public static void main(String[] args) {
+        // we can only throw throwable objects not objects of normal Java Class
+        //throw throwNormalObject; :: results in compile time error
+    }
+}
