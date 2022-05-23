cf47c633ae0cc18d5b9c0828e126612237242383
paras <parasverma2531@gmail.com>
2022-05-24 02:11:03 +0530

    throw keyword

---
+++ ExceptionHandling/throwKeyword.java
@@ -0,0 +1,8 @@
+package ExceptionHandling;
+import java.util.*;
+public class throwKeyword {
+    public static void main(String[] args) {
+        System.out.println("sometimes we may need to create explicit exception and may need to handover it to JVM manually which is done by throw keyword");
+        throw  new java.lang.ArithmeticException("/ by zero");
+    }
+}
