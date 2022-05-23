016cecf59b8d593168860eaa30203a61f31b6951
paras <parasverma2531@gmail.com>
2022-05-24 01:29:45 +0530

    try catch brackets

--- ExceptionHandling/try_catch.java
+++ ExceptionHandling/try_catch.java
@@ -2,8 +2,11 @@ package ExceptionHandling;
 
 public class try_catch {
     public static void main(String[] args) {
-        try // try-catch cannot be used without curly brackets
-        System.out.println(10/0);
+       // try
+        //System.out.println(10 / 0);
+        try {// try-catch cannot be used without curly brackets
+            System.out.println(10 / 0);
+        }
         catch(Exception e)
         {
             System.out.println(e);
