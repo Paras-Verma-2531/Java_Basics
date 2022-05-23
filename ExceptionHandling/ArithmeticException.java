a9de1d5118fefe41915ea06da2aa0a1f0b69285c
paras <parasverma2531@gmail.com>
2022-05-23 23:52:06 +0530

    Arithmetic Exception

---
+++ ExceptionHandling/ArithmeticException.java
@@ -0,0 +1,8 @@
+package ExceptionHandling;
+
+public class ArithmeticException {
+    public static void main(String[] args) {
+        System.out.println(10/0);// Arithmetic Exception (No try-catch block)
+        
+    }
+}
--- ExceptionHandling/checked1.java
+++ ExceptionHandling/checked1.java
@@ -4,8 +4,8 @@ import java.io.PrintWriter;
 
 public class checked1 {
     public static void main(String[] args) {
-        PrintWriter pw = new PrintWriter("abc.txt");// unreported checked exception ( compiler is responsible)
-        pw.println("hello how are you");
+        //PrintWriter pw = new PrintWriter("abc.txt");// unreported checked exception ( compiler is responsible)
+        //pw.println("hello how are you");
         System.out.println("done!!");
 
     }
