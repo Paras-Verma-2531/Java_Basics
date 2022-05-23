c7c0048d07f2e3f97d9dce09fbd61cc8d1594012
paras <parasverma2531@gmail.com>
2022-05-23 23:59:20 +0530

    Exception handled

--- ExceptionHandling/ArithmeticException.java
+++ ExceptionHandling/ArithmeticException.java
@@ -2,7 +2,13 @@ package ExceptionHandling;
 
 public class ArithmeticException {
     public static void main(String[] args) {
-        System.out.println(10/0);// Arithmetic Exception (No try-catch block)
-        
+        //System.out.println(10/0);// Arithmetic Exception (No try-catch block)
+        try {
+            System.out.println(10 / 0);// exception handled
+        } catch (Exception e) {
+
+            System.out.println(e);
+
+        }
     }
 }
