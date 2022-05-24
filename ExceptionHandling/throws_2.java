4f430a3ef6270cf8d2180bf7969960072b17c9a9
paras <parasverma2531@gmail.com>
2022-05-25 00:47:42 +0530

    throws normal type error

---
+++ ExceptionHandling/throws_2.java
@@ -0,0 +1,11 @@
+package ExceptionHandling;
+
+public class throws_2  extends  RuntimeException{
+    //public static void main(String[] args)throws throws_2 {
+        // invalid because we can only  use throws for  throwable types
+    // not applicable for normal java classes:: incompatible types Compile time error
+
+    public static void main(String[] args)throws throws_2 {
+
+    }
+}
