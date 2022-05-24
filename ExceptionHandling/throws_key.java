6135e9a760c2a1d1be9f76d30708a1a7664855f6
paras <parasverma2531@gmail.com>
2022-05-25 00:24:55 +0530

    throws keyword introduction

---
+++ ExceptionHandling/throws_key.java
@@ -0,0 +1,12 @@
+package ExceptionHandling;
+
+public class throws_key {
+    public static void main(String[] args) {
+        /*
+        it is used to delegate the responsibility to the caller method(could be main as well)
+        can only be used for checked Exceptions
+        with unchecked Exception there is no impact..
+        it only convience the compiler and hence does not prevent abnormal termination.
+         */
+    }
+}
