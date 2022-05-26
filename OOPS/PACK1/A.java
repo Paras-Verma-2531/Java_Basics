0025202ff7d489a40365af57c5dd5279707da692
paras <parasverma2531@gmail.com>
2022-05-26 15:27:34 +0530

    OOPS concepts

---
+++ OOPS/PACK1/A.java
@@ -0,0 +1,8 @@
+package OOPS.PACK1;
+public class A {
+    // to access any class outside the same package:: such class should be public
+    protected  void m1()
+    {
+        System.out.println("A class protected method");
+    }
+}
