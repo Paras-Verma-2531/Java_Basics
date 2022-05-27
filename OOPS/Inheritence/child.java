d3de60bf61fceaf510a4aa7d14c94c36f0c7de7b
paras <parasverma2531@gmail.com>
2022-05-27 10:11:37 +0530

    multiple inheritance:

---
+++ OOPS/Inheritence/child.java
@@ -0,0 +1,9 @@
+package OOPS.Inheritence;
+
+//public class child extends Parent1,Parent2{
+    // java does not provide support for multiple inheritance
+  //  public static void main(String[] args) {
+    //    child cl = new child();
+      //  cl.method1();
+    //}
+//}
