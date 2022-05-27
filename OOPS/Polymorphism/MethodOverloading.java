0245bd7d59646d2d0cb6ca4db9ceca3e7329f801
paras <parasverma2531@gmail.com>
2022-05-27 10:49:05 +0530

    Method Overloading

---
+++ OOPS/Polymorphism/MethodOverloading.java
@@ -0,0 +1,24 @@
+package OOPS.Polymorphism;
+
+public class MethodOverloading {
+    public void m1()
+    {
+        System.out.println("no arg method");
+    }
+    public void m1(int a)
+    {
+        System.out.println("int arg method");
+    }
+    public void m1(double a)
+    {
+        System.out.println("double arg method");
+    }
+
+    public static void main(String[] args) {
+        MethodOverloading mo = new MethodOverloading();
+        // In method overloading :: method resolution(which method to call) is done by compiler through reference type
+        mo.m1();
+        mo.m1(12);
+        mo.m1(12.2);
+    }
+}
