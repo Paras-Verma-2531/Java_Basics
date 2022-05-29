5b0e5870229122d39ffa7ac859841aa5f928a6e1
paras <parasverma2531@gmail.com>
2022-05-29 22:03:06 +0530

    Introduction

---
+++ GenericTypes/introduction.java
@@ -0,0 +1,7 @@
+package GenericTypes;
+
+public class introduction {
+    public static void main(String[] args) {
+        System.out.println("this package contains all the work regarding generic classes and types");
+    }
+}
