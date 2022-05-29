e63293c7314601387cf1c85054ea26f7ea805380
paras <parasverma2531@gmail.com>
2022-05-29 23:24:46 +0530

    generic intro

--- GenericTypes/introduction.java
+++ GenericTypes/introduction.java
@@ -2,6 +2,15 @@ package GenericTypes;
 
 public class introduction {
     public static void main(String[] args) {
+        /*
+        Generics are type safe eg:(Arrays)-->only allows particular type but they are fixed in size
+        main objective of generics are to provide type safety to the collections(Arraylist etc)..
+        Generics provide type casting::
+        Type safe::
+        ArrayList al = new ArrayList(); before generic
+        ArrayList<String> al = new ArrayList<String>(); After generic
+
+         */
         System.out.println("this package contains all the work regarding generic classes and types");
     }
 }
