5fa119b83c08b60e7c9109cc205d15fba109d3a6
paras <parasverma2531@gmail.com>
2022-05-31 23:04:48 +0530

    overloading of run method

--- Multithreading/threadMain.java
+++ Multithreading/threadMain.java
@@ -7,6 +7,7 @@ public class threadMain {
         // internally call no arg run method
         /*
         ex.run(); would be treated as normal method hence, no thread would be created
+        // overloading is always possible but start method will always call no arg run method
          */
     }
 }
