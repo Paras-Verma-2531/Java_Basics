9f46e5b1fb4c371aeec08ed7faa445b334118b9b
paras <parasverma2531@gmail.com>
2022-05-31 18:28:44 +0530

    Introduction

--- Multithreading/Intro.java
+++ Multithreading/Intro.java
@@ -8,10 +8,14 @@ public class Intro {
         2. runnable interface
         ::Multitasking: performing several task simultaneously
               ----process based multitasking----
-              each process/task is independent of other task(this is not a programmatic based multitasking)
+              each process/task is independent of other task(they all are different programs)
               ----Thread based multitasking----
               executing several threads/process simultaneously where each part is independent of other but is a part of same program.
                 eg: A program contains 10k lines of code 5k-5k (we can compile both at same time rather than waiting for 1st to compile)
+       ::main purpose of multitasking is to improve the performance of the system
+executing multiple threads at a same time/simultaneously is known as multithreading
+eg:: Animations,video games, server development etc (Applications)
+wherever independent job is there, try to use separate threads
          */
     }
 
