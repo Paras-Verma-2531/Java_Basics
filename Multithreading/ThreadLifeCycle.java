2fab86cab0407da66ab93a766c534cd40530609d
paras <parasverma2531@gmail.com>
2022-05-31 23:47:16 +0530

    Simple life cycle of thread

---
+++ Multithreading/ThreadLifeCycle.java
@@ -0,0 +1,9 @@
+package Multithreading;
+public class ThreadLifeCycle {
+    /*
+    1. new/born state:: whenever new object is created, it is known as new/ born state(Mythread t= new Mythread);)
+    2.whenever  start method is called:: thread enters into runnable/ ready state.
+    3. if a processor is allocated to thread by thread scheduler, it enters into running state.
+    4. once run method is completed, thread enters into dead state
+     */
+}
