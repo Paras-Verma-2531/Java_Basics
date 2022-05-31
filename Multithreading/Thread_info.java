8198f8bc21781d1ee4143c788c9299f923c3cbc9
paras <parasverma2531@gmail.com>
2022-05-31 22:10:45 +0530

    Thread_info

---
+++ Multithreading/Thread_info.java
@@ -0,0 +1,27 @@
+package Multithreading;
+public class Thread_info  {
+    /*
+    Thread is a smallest unit of a process or flow of control.
+    every java program contains at least one thread by default known as main thread
+    can be defined using two ways:
+    extend thread class
+    implement  runnable interface
+    ---- extends thread class
+    :: override run method
+    Job of a Thread:: code written inside the run method
+
+    ----------------------------------------------
+    public class MyThread extends Thread
+    {
+     public void run()
+     {
+     ////code
+     }
+     to execute run method one must call the start method
+     psvm..
+     {
+      MyThread mt= new  MyThread();
+      mt.start();// starting of a thread
+
+     */
+}
