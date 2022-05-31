c05e81fb740e46e6ccc07adbd7212dbe190557a5
paras <parasverma2531@gmail.com>
2022-05-31 22:50:50 +0530

    start method significance

--- Multithreading/Thread_info.java
+++ Multithreading/Thread_info.java
@@ -1,7 +1,7 @@
 package Multithreading;
 public class Thread_info  {
     /*
-    Thread is a smallest unit of a process or flow of control.
+    Thread is a lightweight process or can be defined as  flow of control.
     every java program contains at least one thread by default known as main thread
     can be defined using two ways:
     extend thread class
@@ -22,6 +22,23 @@ public class Thread_info  {
      {
       MyThread mt= new  MyThread();
       mt.start();// starting of a thread
+      // main thread starts child thread
+      --->main method and main thread both are different
+      --> main thread calls main method
+      /////////////////////////
+      1.Thread Scheduler:it is a part of JVM ,and it decided the order in which different threads are to be executed.
+      it schedules the execution of thread (order depend upon JVM to JVM)
+      In Multithreading there is no guarantte for particular output.
+
+      ---->IMPORTANT<----
+
+      2. Diff bw start and run method::
+      start() method is used to create new thread which internally calls run method.
+      if we call run() method instead of start:: then it would be treated as normal method( no thread would be created)
+
+      ----Significance of start method
+      1. it is used to register newly created thread with thread scheduler and all other mandatory activities.
+      hence,without using start method, there is no chance to start new thread. hence, start method is known as heart of multithreading
 
      */
 }
