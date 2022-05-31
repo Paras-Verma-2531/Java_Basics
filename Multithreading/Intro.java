b2c0460715026c48215eb1fd9e6022c9be67ceac
paras <parasverma2531@gmail.com>
2022-05-31 17:59:26 +0530

    Introduction

--- Multithreading/Intro.java
+++ Multithreading/Intro.java
@@ -2,6 +2,17 @@ package Multithreading;
 public class Intro {
     public static void main(String[] args) {
         System.out.println("Multithreading:::");
+        /*
+        MultiThreading can be implemented in two ways:
+        1. extending thread class
+        2. runnable interface
+        ::Multitasking: performing several task simultaneously
+              ----process based multitasking----
+              each process/task is independent of other task(this is not a programmatic based multitasking)
+              ----Thread based multitasking----
+              executing several threads/process simultaneously where each part is independent of other but is a part of same program.
+                eg: A program contains 10k lines of code 5k-5k (we can compile both at same time rather than waiting for 1st to compile)
+         */
     }
 
 }
