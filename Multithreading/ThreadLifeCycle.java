f9fffe61cbdd8f5bf828fcdda03317cd0b8b094f
paras <parasverma2531@gmail.com>
2022-06-01 00:00:27 +0530

    Life cycle of thread & info

--- Multithreading/ThreadLifeCycle.java
+++ Multithreading/ThreadLifeCycle.java
@@ -1,9 +1,12 @@
 package Multithreading;
 public class ThreadLifeCycle {
     /*
+    ---------Simple Life Cycle ----------
     1. new/born state:: whenever new object is created, it is known as new/ born state(Mythread t= new Mythread);)
     2.whenever  start method is called:: thread enters into runnable/ ready state.
     3. if a processor is allocated to thread by thread scheduler, it enters into running state.
     4. once run method is completed, thread enters into dead state
+    ----------
+    once a life cycle of thread is started , if we try to start it again we'll be getting runtime exception (IllegalThreadState exception)
      */
 }
