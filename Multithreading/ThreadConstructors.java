10dd1fbc4f60dc5f43c6fbf327b769e76b0e9c78
paras <parasverma2531@gmail.com>
2022-06-01 23:40:35 +0530

    thread constructor

---
+++ Multithreading/ThreadConstructors.java
@@ -0,0 +1,15 @@
+package Multithreading;
+
+public class ThreadConstructors extends Thread{
+    public static void main(String[] args) {
+        /*
+        Thread t= new Thread();
+        Thread t= new Thread(Runnable r);
+        Thread t= new Thread("name");
+        Thread t= new Thread(Runnable r,String name);
+        Thread t= new Thread(ThreadGroup g, String name);
+        Thread t= new Thread(ThreadGroup g,Runnable r);
+        Thread t= new Thread(ThreadGroup g,Runnable r,String name);
+         */
+    }
+}
