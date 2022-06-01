f36150eabced8aa09d3900f1033a93a34955736c
paras <parasverma2531@gmail.com>
2022-06-02 01:05:08 +0530

    thread priority

--- Multithreading/ThreadPriority.java
+++ Multithreading/ThreadPriority.java
@@ -9,12 +9,15 @@ public class ThreadPriority extends Thread {
     :)Thread.MIN_PRIORITY=1
     :)Thread.MAX_PRIORITY=10
     :)Thread.NORM_PRIORITY=5
-
+     default priority for main thread is :: 5
+     and for other it would be inherited from parent thread i,e same as parent thread
      */
     public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getPriority());
         ThreadPriority tp = new ThreadPriority();
+        System.out.println(tp.getPriority());// would be same as its parent thread 
         tp.setPriority(9);// set priority
         tp.setName("paras thread");
-        System.out.println("name of the thread is " +tp.getName()+" and its priority is "+tp.getPriority());
+        System.out.println("Name of the thread is " +tp.getName()+" and its priority is "+tp.getPriority());
     }
 }
