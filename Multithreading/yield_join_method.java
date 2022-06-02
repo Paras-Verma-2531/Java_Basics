415db475bacef3e7ff378c930da52c75a92de843
paras <parasverma2531@gmail.com>
2022-06-02 13:25:47 +0530

    yield method

--- Multithreading/yield_join_method.java
+++ Multithreading/yield_join_method.java
@@ -1,5 +1,5 @@
 package Multithreading;
-public class yield_join_method {
+public class yield_join_method extends Thread {
     /*
     methods which can be used to prevent thread execution are:
     yield()
@@ -8,5 +8,14 @@ public class yield_join_method {
     :: yield() method:-->:
     it pauses the current working thread to give chance to waiting threads of same priority
     if there is no waiting thread , same thread can continue its execution
+    yield method rollback the thread from running state to runnable/ready state
      */
+    public void run()
+    {
+        for(int i=0;i<10;i++)
+        {
+            System.out.println("child thread::");
+            Thread.yield();// pauses the current working thread
+        }
+    }
 }
