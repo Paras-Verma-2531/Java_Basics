344889a4715209e1ca163e84a12b0888d9710656
paras <parasverma2531@gmail.com>
2022-06-03 00:14:33 +0530

    Synchronization intro

---
+++ Multithreading/Synchronization_intro.java
@@ -0,0 +1,7 @@
+package Multithreading;
+public class Synchronization_intro {
+    /*
+    the biggest advantage of synchronized keyword is it prevents data inconsistency problem.
+    main disadvantage is it increases waiting time of thread and creates performance problem.
+     */
+}
