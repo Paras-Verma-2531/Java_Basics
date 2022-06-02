8e6991cf2a31f5825048b4f5e386fdccd3cb240f
paras <parasverma2531@gmail.com>
2022-06-03 00:35:22 +0530

    Singleton approach 1 & 2

--- Singleton/SingletonImplementation.java
+++ Singleton/SingletonImplementation.java
@@ -2,6 +2,7 @@ package Singleton;
 
 public class SingletonImplementation {
     // Approach 1::
+    /*
    private static SingletonImplementation instance= new SingletonImplementation();
     private SingletonImplementation()
     {}
@@ -9,4 +10,17 @@ public class SingletonImplementation {
     {
         return instance;
     }
+    */
+    //Approach 2::
+    private static SingletonImplementation instance =null;
+    // no wastage of object at the beginning
+    private SingletonImplementation()
+    {}
+    public static SingletonImplementation getInstance()
+    {
+        if(instance==null) {
+            instance = new SingletonImplementation();
+        }return instance;
+    }
+
 }
--- Singleton/Singleton_main.java
+++ Singleton/Singleton_main.java
@@ -3,7 +3,7 @@ public class Singleton_main {
     public static void main(String[] args) {
         SingletonImplementation instance= SingletonImplementation.getInstance();// 1st object created
         SingletonImplementation instance1= SingletonImplementation.getInstance();// uses same reference
-        
+
 
 
     }
