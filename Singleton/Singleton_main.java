584f476982bba548098fff0397eb177e4cc0e7c4
paras <parasverma2531@gmail.com>
2022-06-03 00:30:40 +0530

    Singleton main

---
+++ Singleton/Singleton_main.java
@@ -0,0 +1,10 @@
+package Singleton;
+public class Singleton_main {
+    public static void main(String[] args) {
+        SingletonImplementation instance= SingletonImplementation.getInstance();// 1st object created
+        SingletonImplementation instance1= SingletonImplementation.getInstance();// uses same reference
+        
+
+
+    }
+}
