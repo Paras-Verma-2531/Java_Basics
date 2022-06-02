26081be4db3c7b78518c6cd0162b4970c481d36c
paras <parasverma2531@gmail.com>
2022-06-02 18:12:23 +0530

    join method info

--- Multithreading/join_method.java
+++ Multithreading/join_method.java
@@ -10,5 +10,12 @@ public class join_method {
     i,e----> t2 will call join method on t1 [t1.join()]
     similarly, delivering card has to wait for t2
     ----> t3 will call join method on t2 [t2.join()]
+     [NOTE]::every join method throws InterruptedException which is checked exception::
+     hence, it should be handled by using try catch or throws keyword
+    --------- prototype of join method -----------
+     ::public final void join()
+     ::public final void join(long ml)-->millisecond
+     ::public final void join(long ml,int ns)-->nanosecond
+
      */
 }
