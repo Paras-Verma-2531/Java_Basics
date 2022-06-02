379c02de0ef50e07ef3c2f3a13fe552288f83d58
paras <parasverma2531@gmail.com>
2022-06-03 00:46:11 +0530

    Singleton main

--- Singleton/Singleton_main.java
+++ Singleton/Singleton_main.java
@@ -3,7 +3,8 @@ public class Singleton_main {
     public static void main(String[] args) {
         SingletonImplementation instance= SingletonImplementation.getInstance();// 1st object created
         SingletonImplementation instance1= SingletonImplementation.getInstance();// uses same reference
-
+        System.out.println(instance.toString());
+        System.out.println(instance1.toString());//same reference
 
 
     }
