7e7cb41c9eb74f3eb653855bf83fe96671161c4a
paras <parasverma2531@gmail.com>
2022-06-03 22:16:26 +0530

    Innerclass intro

---
+++ InnerClass/intro.java
@@ -0,0 +1,19 @@
+package InnerClass;
+public class intro {
+    /*
+    Classes inside the classes are known as InnerClasses.
+    The main purpose of innerclass were to fix the bug  of GUI in event handling.
+    however, it became so popular that programmers started using it in regular coding.
+    -----when to use innerclass concept-------
+    without existing one type of object, if there is no chance for other object:: then we can use innerclass
+    eg::
+    class university
+    {
+       class department
+       {
+       }
+
+     }
+     here department depends on uni:: hence it is inner class
+     */
+}
