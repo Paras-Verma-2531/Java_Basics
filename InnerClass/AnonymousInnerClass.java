6c531d3ae59ce45c8e071a11b25f646fac5f9e96
paras <parasverma2531@gmail.com>
2022-06-05 18:53:53 +0530

    Anonymous Innerclass Intro

---
+++ InnerClass/AnonymousInnerClass.java
@@ -0,0 +1,24 @@
+package InnerClass;
+public class AnonymousInnerClass {
+    /*
+    InnerClass without name are known as Anonymous InnerClass.
+    they are the wildly used InnerClass.
+    Main purpose::
+    -->instant usage/ one time use.
+    [[There are 3 types of Anonymous InnerClass:]]
+    1. Anonymous Innerclass which extends another class
+    2. Anonymous Innerclass which implements interface.
+    3. Anonymous Innerclass that defined inside arguments.
+    ------------------------------------------------------------
+       [[ Anonymous Innerclass which extends another class]]
+       syntax:
+       normal syntax: popcorn p= new popcorn();
+      -------> Anonymous class which extends popcorn:<------
+       popcorn p = new popcorn()
+       {
+       }; point to remember:: although we have parent class name for object creation, still it is an object for Anonymous class
+       Thread t= new Thread()
+       {
+       }; [Anonymous class which extends thread class like mythread extends thread]
+     */
+}
