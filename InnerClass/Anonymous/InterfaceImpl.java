b89d9ceef83bf886a7905aa27bd31b18f0a46fb7
paras <parasverma2531@gmail.com>
2022-06-06 09:23:14 +0530

    Anonymous Innerclass

---
+++ InnerClass/Anonymous/InterfaceImpl.java
@@ -0,0 +1,23 @@
+package InnerClass.Anonymous;
+public class InterfaceImpl {
+    // defining a thread by implementing runnable interface
+    public static void main(String[] args) {
+        Runnable rn = new Runnable() {
+            public void run()
+            {
+                for(int i=0;i<10;i++)
+                {
+                    System.out.println("thread through runnable interface (Anonymous class)");
+                    try{
+                        Thread.sleep(2000);
+                    }catch (InterruptedException e)
+                    {
+                        System.out.println(e);
+                    }
+                }
+            }
+        };
+        Thread th= new Thread(rn);
+        th.start();
+    }
+}
