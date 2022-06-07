6f335810b9d9a31dad1a6441ae5e0a3742632847
paras <parasverma2531@gmail.com>
2022-06-07 11:16:44 +0530

    introduction

--- FileHandling/intro.java
+++ FileHandling/intro.java
@@ -21,7 +21,16 @@ public class intro {
     File f= new File("file name");->for current working directory
     File f= new File(String subdir,"file name");----> to  store at other directory
     File f= new File(file subdir,"file name");-----> in addition to 2nd constr, if a file is being referenced to that loc, use that file reference
-
+---------------------------------------IMP METHODS IN FILE CLASS-----------------------------------
+methods used in File
+createNewFile();
+.isFile();
+.isDirectory();
+makedir();
+exists();
+list();------->to list out all the files
+length()--->returns the length of file(return long type)
+f.delete();
 
      */
 }
