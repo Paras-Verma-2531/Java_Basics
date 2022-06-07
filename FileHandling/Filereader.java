c65177bbf57fdc1bffff78d1074d9801c1942210
paras <parasverma2531@gmail.com>
2022-06-07 22:47:50 +0530

    FileReader

---
+++ FileHandling/Filereader.java
@@ -0,0 +1,21 @@
+package FileHandling;
+public class Filereader {
+    /*
+    we  can use fileReader to read character data from the file.
+    -----------------------------
+    constructors used in FileReader class::
+    1.FileReader fr= new FileReader(String fname);
+    2.FileReader fr= new FileReader(file reference);
+    --------------------------
+    --------------------------
+    methods use in FileReader class::
+    1. read()--> reads character by character[ return type is int](unicode value)
+    2.int read(char[] ch)---> to read char array[ it returns no of character copied from file to array]
+    ex:: FileReader fr= new FileReader("abc.txt");
+    char[] ch= new char[10000];
+    let assume abc has 1000 characters
+    int size=fr.read(ch);
+    here, size is 1000.
+
+     */
+}
