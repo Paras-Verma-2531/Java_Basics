76d279bf1aea89ef66353e46492aa5cc9b244fd1
paras <parasverma2531@gmail.com>
2022-06-07 18:14:36 +0530

    FileWriter class

--- FileHandling/FileWriter_impl.java
+++ FileHandling/FileWriter_impl.java
@@ -1,4 +1,8 @@
 package FileHandling;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileWriter_impl {
     /*
     in order to write we can use FileWriter class:
@@ -16,6 +20,17 @@ public class FileWriter_impl {
      Method present in FileWriter class:
      1. write(int ch);-------> A character
      2. write(char[] ch);-----> char array
-     2. write(String s);------> to write string data
+     3. write(String s);------> to write string data
+     4. flush()--> it gives the guarantee that all the characters have been written to the file
+     5. close()--> to close the writer
      */
+    public static void main(String[] args) throws IOException {
+        FileWriter fw= new FileWriter("abc.txt");
+        fw.write(100);
+        fw.write("efd\n");
+        char[] ch={'a','b','c'};
+        fw.write(ch);
+        fw.close();
+
+    }
 }
