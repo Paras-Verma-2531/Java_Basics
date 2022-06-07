bd08aac1615d313c2f1e01043ea898fdc3c82347
paras <parasverma2531@gmail.com>
2022-06-07 11:00:01 +0530

    introduction

---
+++ FileHandling/intro.java
@@ -0,0 +1,27 @@
+package FileHandling;
+public class intro {
+    /*
+    sometimes, we may require to store output  permanently for which file handling comes into picture.
+    Classes we need to study in File Handling are::
+    --------------------
+    1.File
+    2.FileWriter
+    3.FileReader
+    4.BufferedWriter
+    5.BufferedReader
+    6.PrintWriter
+    ---------------------------------
+    how to create a new file
+    -------------------------------
+    File f= new File("filename.txt");
+    f.createNewFile();
+    -------------------------------
+    -------------------------------
+    File class constructor::
+    File f= new File("file name");->for current working directory
+    File f= new File(String subdir,"file name");----> to  store at other directory
+    File f= new File(file subdir,"file name");-----> in addition to 2nd constr, if a file is being referenced to that loc, use that file reference
+
+
+     */
+}
