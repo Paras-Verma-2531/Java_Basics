258db6b92407e439cee63758e38e8ffc3ee6ee5e
paras <parasverma2531@gmail.com>
2022-06-09 18:15:55 +0530

    Introduction

---
+++ RegularExpression/Intro.java
@@ -0,0 +1,10 @@
+package RegularExpression;
+public class Intro {
+    /*
+    whenever, we are required to represent group of strings in accordance with some pattern. Then we should go for Regular Expressions.
+    ----------------
+    Applications of Regular Expressions:
+    1. Validation frameworks in forms(enquiry/registration form etc)--> email validation(checks the format).
+    2. Pattern matching applications.
+     */
+}
