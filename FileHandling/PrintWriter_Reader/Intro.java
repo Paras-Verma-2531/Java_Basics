d429c46322a19fa6fdacee304c33dfb21f3f4d9d
paras <parasverma2531@gmail.com>
2022-06-09 09:42:13 +0530

    intro

---
+++ FileHandling/PrintWriter_Reader/Intro.java
@@ -0,0 +1,34 @@
+package FileHandling.PrintWriter_Reader;
+public class Intro {
+    /*
+    The problem faced by FileWriter and BufferedWriter are:
+    it was difficult to insert new line(every time use \n")
+    .solved by BufferedWriter by newLine() method
+    but still it is a major problem.
+    Also, we cannot write double/int/boolean data to the file.therefore, PrintWriter  class was introduced.
+    -------------------------
+    -------------------------
+    The major advantage of printWriter over other Writers are:
+    ::)we can write any primitive data to the file.
+
+    -------------------
+    -------------------
+    constructors of PrintWriter
+    --------------
+    --------------
+    PrintWriter pw= new PrintWriter("file name");
+    PrintWriter pw= new PrintWriter("file reference");
+    PrintWriter pw= new PrintWriter("Writer object");--- > as like BufferedWriter
+    [NOTE]:: PrintWriter can communicate directly with the file or with the help of some other Writer Object.
+=================================
+=================================
+-------METHODS---------
+1.print()-----> to write sny data to file (primitive/ non-primitive)
+2.println()---> to write data with new Line.
+---------------
+The most enhanced writer  to write data to the file is PrintWriter.
+The most enhanced Reader to read data from the file is BufferedReader.
+
+
+     */
+}
