a2665cac5489389818459abd64d9a32c818e6357
paras <parasverma2531@gmail.com>
2022-06-13 10:36:31 +0530

    introduction

---
+++ ADVANCE_JAVA/Intro.java
@@ -0,0 +1,24 @@
+package ADVANCE_JAVA;
+public class Intro {
+    /*
+    Core Java concepts can be used to deign stand-alone applications.
+    Stand-alone applications include:
+    [Applications that require single computer for its execution]
+    =======================
+    1.GUI  applications[ desktop applications]
+    2.CUI applications[ character user interface, console based applications ]
+    ======================================
+    Sometimes, we may require building web applications. for which knowledge of core java is not sufficient. Therefore, advanced java concepts
+    are mandatory for Web based applications.
+    like :: websites, servers etc
+==============================================
+==============================================
+Advanced Java majorly include three technologies:
+        1. JDBC
+        2.SERVLETS
+        3.JSPs
+       [[The main purpose of these technologies is to develop web based applications.]]
+
+
+     */
+}
