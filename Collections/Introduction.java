9fa74d392bb73bee46ac023b5a3399b46f0f0588
paras <parasverma2531@gmail.com>
2022-06-15 23:01:00 +0530

    intro

---
+++ Collections/Introduction.java
@@ -0,0 +1,14 @@
+package Collections;
+public class Introduction {
+    /*
+       An array is an indexed of fixed no of homo data elements.
+       the main advantage of arrays is we can represent values with same variable:[code readability]
+       Disadvantages::
+        : fixed size
+        : homogeneous
+        : no inbuilt method
+        =======================
+        
+
+     */
+}
