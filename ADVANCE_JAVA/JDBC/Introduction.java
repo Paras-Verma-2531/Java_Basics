47f3ae611654ea194fa9308a03c814e0f70487ea
paras <parasverma2531@gmail.com>
2022-06-15 10:00:40 +0530

    JDBC intro/classpath

---
+++ ADVANCE_JAVA/JDBC/Introduction.java
@@ -0,0 +1,21 @@
+package ADVANCE_JAVA.JDBC;
+
+public class Introduction {
+    /*
+    JDBC stands for JAVA DATABASE CONNECTIVITY . it is a Java based API which is used to interact with various
+    set of databases.
+    JDBC is a mediator which helps the java program/app to interact with database.
+    prerequisite steps::
+    download mysql installer
+    set up the installer(server,shell,workbench)
+    set up the root password
+    download JDBC connector.jar file
+    set class path
+    class path-->[path address];.;
+    run the command:: javap com.mysql.jdbc.Driver on cmd
+
+     */
+
+}
+
+
