2966b73ed59ddfd72a79226fd02b8d0405d6cb22
paras <parasverma2531@gmail.com>
2022-06-16 22:53:12 +0530

    common method

---
+++ Collections/CommonMethods.java
@@ -0,0 +1,24 @@
+package Collections;
+public class CommonMethods {
+    /*
+    1.boolean add(object o)
+    2.boolean addAll(collection c)
+    =======================
+    3.boolean remove(object o)
+    4.boolean removeAll(collection c)
+    5.void clear()
+    6.boolean retainAll(collection c)===>except these all other will be removed:
+    ==================================
+    7.boolean contains(object o)
+    8.boolean containsAll(collection c)
+    9.int size()
+    10.boolean isEmpty()
+    ========================
+  ::::  [to traverse in collection we need iterator method::]
+    [which returns iterator object:]
+    Iterator iterator()
+    =======================================
+
+
+     */
+}
