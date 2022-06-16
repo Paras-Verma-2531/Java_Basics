73bb090b51e6e248c78040eff1a43d4f25a35fe8
paras <parasverma2531@gmail.com>
2022-06-16 23:16:49 +0530

    ListInterface

---
+++ Collections/ListInterface.java
@@ -0,0 +1,24 @@
+package Collections;
+
+public class ListInterface {
+    /*
+    List is a child interface of Collection
+    whenever we are supposed to add elements where duplicates are allowed and order of insertion is preserved. then we should
+    go for List.
+    ======================
+    methods of List are:
+    1. void add(int index,object o);
+    2.boolean addAll(int index,collection c);
+    3.Object get(int index);
+    4.Object remove(int index);
+    5. Object set(int index,object new);// sets new element at that location
+    6.int indexOf(object o)// returns first occurrence
+    7.int lastIndexOf(object o);
+    ======================================
+    Various implementation classes of List are:
+    1. ArrayList
+    2.LinkedList
+    3.Stack
+      |.Vector
+     */
+}
