a25daec39596cbda136972a9af2b3cd2fc902d1a
paras <parasverma2531@gmail.com>
2022-06-18 00:43:55 +0530

    ArrayList

---
+++ Collections/ArrayList.java
@@ -0,0 +1,9 @@
+package Collections;
+public class ArrayList {
+    /*
+    ArrayList is best when we are required to retrieve element, and it is worse when we need to continuously add data(somewhere in the middle).
+    deletion(somewhere in the middle):
+
+
+     */
+}
