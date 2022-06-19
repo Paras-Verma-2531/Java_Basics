de8f52de17863e4e59dff74b0f894636fb1ac27c
paras <parasverma2531@gmail.com>
2022-06-19 11:56:00 +0530

    Arraylist/LiinkedList

--- Collections/ArrayList.java
+++ Collections/ArrayList.java
@@ -3,7 +3,7 @@ public class ArrayList {
     /*
     ArrayList is best when we are required to retrieve element, and it is worse when we need to continuously add data(somewhere in the middle).
     deletion(somewhere in the middle):
-
+     LinkedList is the best suitable for insertion and deletion but is worse for retrieval of data at constant basis.
 
      */
 }
