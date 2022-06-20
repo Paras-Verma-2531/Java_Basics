5f38440205e073f48c7f0b23550ae6b9992afb8d
paras <parasverma2531@gmail.com>
2022-06-20 11:23:21 +0530

    UpdateDatabase

--- ADVANCE_JAVA/JDBC/UpdateDatabase.java
+++ ADVANCE_JAVA/JDBC/UpdateDatabase.java
@@ -7,12 +7,15 @@ import java.util.Scanner;
 public class UpdateDatabase  {
     public static void main(String[] args) throws SQLException,ClassNotFoundException {
         Connection con= new ReturnConnection().returnConnection();
-        String query="update student set name=? where name='Paras'";//PreparedStatement
+        String query="update student set name=? where name=?";//PreparedStatement
         PreparedStatement ptm= con.prepareStatement(query);
         Scanner in= new Scanner(System.in);
         System.out.print("enter the new name : ");
         String newname= in.nextLine();
+        System.out.print("enter the previous name : ");
+        String oldname= in.nextLine();
         ptm.setString(1,newname);
+        ptm.setString(2,oldname);
         ptm.executeUpdate();
         System.out.println("successfully updated the name");
 
