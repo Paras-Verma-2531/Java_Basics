590a9a673aff94c440eca65e3b2dbca587a5d0e6
paras <parasverma2531@gmail.com>
2022-06-21 23:00:50 +0530

    Set/Introduction

---
+++ Collections/Set/Introduction.java
@@ -0,0 +1,15 @@
+package Collections.Set;
+public class Introduction {
+    /*
+    Set is an interface which implements collections.
+    Set is implemented by following classes;
+    :TreeSet
+    :HashSet
+    :LinkedHashSet
+    ==============================
+    Set acquires the unique property:
+      -> it can hold only distinct elements
+      -> elements are unordered
+      
+     */
+}
