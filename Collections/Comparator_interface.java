c88bd681d824e5cdfd785d1c5a918119617000bd
paras <parasverma2531@gmail.com>
2022-06-23 05:00:18 +0530

    Comparator interface

--- Collections/Comparator_interface.java
+++ Collections/Comparator_interface.java
@@ -1,7 +1,13 @@
 package Collections;
+
+import java.util.Comparator;
+import java.util.TreeSet;
+
 public class Comparator_interface {
     /*
      Comparator interface is present in java.util package, it defines 2 methods:
+     // whenever we are required to go with customized sorting: go with Comparator[sorting in descending order]
+
      -------------------
      compare()===> method
      equals()===>method
@@ -14,4 +20,24 @@ public class Comparator_interface {
      =========================
      public boolean equals(object ob1)
      */
+    public static void main(String[] args) {
+        // customised sorting : therefore used Comparator
+        TreeSet<Integer>ts= new TreeSet<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if(o1>o2)
+                    return -1;
+                else if(o1<o2)
+                    return 1;
+                else
+                    return  0;
+            }
+        });
+        ts.add(10);
+        ts.add(0);
+        ts.add(20);
+        ts.add(5);
+        ts.add(50);
+        System.out.println(ts);
+    }
 }
