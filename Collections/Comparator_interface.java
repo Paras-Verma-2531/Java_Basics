2f8bbb66f74466e048cf0b718aed37cffe1c44ce
paras <parasverma2531@gmail.com>
2022-06-23 04:40:23 +0530

    Comparator interface

---
+++ Collections/Comparator_interface.java
@@ -0,0 +1,17 @@
+package Collections;
+public class Comparator_interface {
+    /*
+     Comparator interface is present in java.util package, it defines 2 methods:
+     -------------------
+     compare()===> method
+     equals()===>method
+     ======================
+     compare() method::
+     public int compare(object ob1,object ob2)
+     returns -ve [ob1<ob2]
+     returns +ve[ob1>ob2]
+     returns 0[ob1==ob2]
+     =========================
+     public boolean equals(object ob1)
+     */
+}
