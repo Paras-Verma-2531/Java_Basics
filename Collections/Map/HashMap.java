55eb78fbe6fb212377b33a25a1859f3e5586e4fa
paras <parasverma2531@gmail.com>
2022-06-23 05:45:25 +0530

    HashMap

---
+++ Collections/Map/HashMap.java
@@ -0,0 +1,14 @@
+package Collections.Map;
+
+public class HashMap {
+    /*
+    Map is an interface which is used whenever we are required to work with key value pairs;
+    ==== it is not a child interface of collection::====
+    -------------------------------------
+    the classes which implements Map are:
+    ----------these are the majorly used classes---------------
+    1.HashMap
+    2.LinkedHashMap
+    3.SortedMap
+     */
+}
