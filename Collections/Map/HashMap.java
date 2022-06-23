d056e429b8487b6b7b7592002d797992dad405ac
paras <parasverma2531@gmail.com>
2022-06-23 17:32:16 +0530

    HashMap

--- Collections/Map/HashMap.java
+++ Collections/Map/HashMap.java
@@ -10,5 +10,18 @@ public class HashMap {
     1.HashMap
     2.LinkedHashMap
     3.SortedMap
+    ===========================
+    Methods used in HashMap::
+    ---------------------------
+    1.put(object key,object value)
+    2.putAll(map m);
+    3.get(key);---> returns value
+    4.remove(key);
+    5.containsKey(key);
+    6.containsValue(value);
+    7.remove(key);
+    8.isEmpty();
+    9.size();
+    10.clear();
      */
 }
