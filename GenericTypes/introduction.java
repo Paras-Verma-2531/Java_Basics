f266d747357603b3e31648f33b57fa91da3ae63c
paras <parasverma2531@gmail.com>
2022-06-23 17:28:07 +0530

    Introduction

--- GenericTypes/introduction.java
+++ GenericTypes/introduction.java
@@ -7,8 +7,8 @@ public class introduction {
         main objective of generics are to provide type safety to the collections(Arraylist etc)..
         Generics provide type casting::
         Type safe::
-        ArrayList al = new ArrayList(); before generic
-        ArrayList<String> al = new ArrayList<String>(); After generic
+        ArrayList_imp al = new ArrayList_imp(); before generic
+        ArrayList_imp<String> al = new ArrayList_imp<String>(); After generic
 
          */
         System.out.println("this package contains all the work regarding generic classes and types");
