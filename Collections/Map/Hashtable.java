56f44119c6dc359c32af505402a2247946cd8fa3
paras <parasverma2531@gmail.com>
2022-06-28 23:33:18 +0530

    Hashtable

---
+++ Collections/Map/Hashtable.java
@@ -0,0 +1,13 @@
+package Collections.Map;
+public class Hashtable {
+    /*
+    Constructors:
+    --------------
+    1.Hashtable ht= new Hashtable();// initial capacity=11, fill ratio 0.75
+    2.Hashtable ht= new Hashtable(int capacity);// modified capacity
+    3.Hashtable ht= new Hashtable(int capacity,float ff);// fill factor
+    4.Hashtable ht= new Hashtable(Map m);// Map object
+
+
+     */
+}
