e1fddf5c1cc039a444e1b2dd6101fbf00ee067e3
paras <parasverma2531@gmail.com>
2022-07-25 19:28:19 +0530

    Connection Setup

--- ADVANCE_JAVA/JDBC/Connection_setup.java
+++ ADVANCE_JAVA/JDBC/Connection_setup.java
@@ -1,6 +1,7 @@
 package ADVANCE_JAVA.JDBC;
 import java.sql.*;
 public class Connection_setup {
+
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         //load the driver
         Class.forName("com.mysql.cj.jdbc.Driver");// driver name
@@ -8,11 +9,8 @@ public class Connection_setup {
         Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc","root","vd8DYewD@1");//url,username,passowrd
         // make query
         if(con.isClosed())
-        {
             System.out.println("connection is still closed");
-        }else
-        {
+        else
             System.out.println("connection is open");
-        }
     }
 }
