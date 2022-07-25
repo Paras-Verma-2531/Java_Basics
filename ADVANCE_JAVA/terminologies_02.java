884d774dc286aceb2ea7b6a1faade2b130f5ed83
paras <parasverma2531@gmail.com>
2022-07-25 19:06:52 +0530

    terminologies JSPs/Servlets/JDBC

--- ADVANCE_JAVA/terminologies_02.java
+++ ADVANCE_JAVA/terminologies_02.java
@@ -5,13 +5,13 @@ public class terminologies_02 {
     ROLE OF JSPs ,SERVLETS,JDBC:::
     =======================================
     1.whenever we are required to display something to the end user::
-     go for JSPs [ JAVA SERVER PAGES]// presentation logic
+     go for JSPs [ JAVA SERVER PAGES]// represents the presentation logic
      JSPs are meant for view components.
-     eg: waiter showing menu to the customer in restaurant.
+     eg: waiter showing menu to the customer in Restaurant.
      -----------------------------
      2. the internal processing is done by Servlets.
      eg: cookServlets  [someone who will prepare order and do the processing but won't be seen by the customer.i,e he will be doing the
-     work internally]
+     work internally]//works internally[back-end]
      Hence, Servlets are used for Internal Processing:
      -----------------------------
      3.     If the servlets require some data/ items from the database, it will contact through JDBC.
