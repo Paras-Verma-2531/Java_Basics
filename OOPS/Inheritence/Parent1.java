a6e64c534cc62b8ae7fd92b7262bed7d4e58ce3b
paras <parasverma2531@gmail.com>
2022-08-15 19:34:00 +0530

    Inheritence

--- OOPS/Inheritence/Parent1.java
+++ OOPS/Inheritence/Parent1.java
@@ -1,6 +1,7 @@
 package OOPS.Inheritence;
 
 public class Parent1 {
+    // A parent class which contains method1()
     public void method1()
     {
         System.out.println("method 1 of parent 1");
