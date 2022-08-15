645e7330207be545bf6fdfa9cdb42a21738581a8
paras <parasverma2531@gmail.com>
2022-08-15 19:31:53 +0530

    child

--- OOPS/Inheritence/child.java
+++ OOPS/Inheritence/child.java
@@ -1,5 +1,4 @@
 package OOPS.Inheritence;
-
 //public class child extends Parent1,Parent2{
     // java does not provide support for multiple inheritance
   //  public static void main(String[] args) {
