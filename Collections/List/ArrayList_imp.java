5b818517a5639ba9a19b5e5bdbe29dca7975d3e8
paras <parasverma2531@gmail.com>
2022-08-21 14:38:31 +0530

    ArrayList_imp

--- Collections/List/ArrayList_imp.java
+++ Collections/List/ArrayList_imp.java
@@ -7,8 +7,7 @@ public class ArrayList_imp {
     ArrayList_imp is best when we are required to retrieve element, and it is worse when we need to continuously add data(somewhere in the middle).
     deletion(somewhere in the middle):
      LinkedList is the best suitable for insertion and deletion but is worse for retrieval of data at constant basis.
-
-     */
+    */
     public static void main(String[] args) {
         ArrayList<Integer>list= new ArrayList<>();
         list.add(10);
