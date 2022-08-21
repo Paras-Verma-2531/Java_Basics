238986d8016f03f002949b45de1413b819be3de7
paras <parasverma2531@gmail.com>
2022-08-21 15:07:46 +0530

    ArrayList_imp

--- Collections/List/ArrayList_imp.java
+++ Collections/List/ArrayList_imp.java
@@ -1,6 +1,6 @@
 package Collections.List;
-
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 public class ArrayList_imp {
     /*
@@ -8,6 +8,17 @@ public class ArrayList_imp {
     deletion(somewhere in the middle):
      LinkedList is the best suitable for insertion and deletion but is worse for retrieval of data at constant basis.
     */
+    static void swap(int[] arr,int start,int end)
+    {
+        arr[start]^=arr[end];
+        arr[end]^=arr[start];
+        arr[start]^=arr[end];
+    }
+    static void reverse(int[] arr,int start,int end)
+    {
+        while(start<end)
+            swap(arr,start++,end--);
+    }
     public static void main(String[] args) {
         ArrayList<Integer>list= new ArrayList<>();
         list.add(10);
@@ -23,5 +34,9 @@ public class ArrayList_imp {
         Iterator<Integer>it=list.iterator();
         while(it.hasNext())
             System.out.println(it.next());
+        int[] arr={1,2,3,4,5};
+        //reverse the array:
+        reverse(arr,0,arr.length-1);
+        System.out.println(Arrays.toString(arr));
     }
 }
