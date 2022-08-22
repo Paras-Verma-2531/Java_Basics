e739449a969d7f8ce2d0e40a384f843e85b3b4ba
paras <parasverma2531@gmail.com>
2022-08-22 16:52:24 +0530

    Interface_1

--- Interfaces_prac/Adapter_class/Interface1.java
+++ Interfaces_prac/Adapter_class/Interface1.java
@@ -1,6 +1,9 @@
 package Interfaces_prac.Adapter_class;
 public interface Interface1 {
-    // interface with 7 methods
+    /*interface with 7 methods
+    Since it is mandatory to implement all the methods of interface. But what, if we only need to use one or more method of interface
+    In such cases, Adapter Classes comes into picture.
+     */
     void feature1();
     void feature2();
     void feature3();
