f4c4e592eec2bc2ff2fbadd7bde8ddc0f123586a
paras <parasverma2531@gmail.com>
2022-08-22 16:43:10 +0530

    Adapter_class

--- Interfaces_prac/Adapter_class/intro.java
+++ Interfaces_prac/Adapter_class/intro.java
@@ -1,5 +1,4 @@
 package Interfaces_prac.Adapter_class;
-
 public class intro {
     public static void main(String[] args) {
         System.out.println("class which implements an interface i,e only provide empty implementation of methods and can be used by other classes which only wants one or 2 methods");
