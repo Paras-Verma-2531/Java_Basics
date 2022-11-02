5a4aded00e20e6d047efcf89445fb008672a3375
paras <parasverma2531@gmail.com>
2022-11-03 00:56:06 +0530

    Methodlocal

--- InnerClass/MethodLocal.java
+++ InnerClass/MethodLocal.java
@@ -2,14 +2,19 @@ package InnerClass;
 public class MethodLocal {
     /*
     MOST RARELY INNER TYPE CLASS::
+    ==============================
+    -------------------------------------------
+    ==============================
     sometimes, we can declare a class inside a local class, such class is known as MethodLocal innerclass.
     the main objective of MethodLocal class is as follows::
     suppose we need a method inside a method but in java nested method is not possible. but we have 2 possible ways to resolve this::
-    1. declare that method outside that method( but method is only used inside another method, so why should we declare it in outside the calling method)
+    1. declare that method outside that method( but  this method is only used inside one method, so why should we declare it in outside the calling method)
     2. we can create inner class inside a method and can declare that method inside inner class.
 
     ----------------main purpose:::
     [...it provides method specific repeated functionality...]
+    ==================================
+    
     best suitable---->>[wherever nested method is required, we can go for methodLocal innerclass]
     ::::--->we can access both the static and non-static variables from methodLocal innerclass if a method is instance method.
     ::--> bu if the method is static, we will be getting some compile time error.
