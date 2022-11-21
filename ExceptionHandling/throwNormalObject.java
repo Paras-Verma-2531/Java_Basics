1dfa11042ed115a673d55d6599c458da862bfc32
paras <parasverma2531@gmail.com>
2022-11-21 23:43:12 +0530

    ThrowNormalJavaObject

--- ExceptionHandling/throwNormalObject.java
+++ ExceptionHandling/throwNormalObject.java
@@ -1,9 +1,8 @@
 package ExceptionHandling;
-
 public class throwNormalObject extends RuntimeException {
     public static void main(String[] args) {
         // we can only throw throwable objects not objects of normal Java Class
         //throw  new throwNormalObject; :: results in compile time error
-        throw  new throwNormalObject();// cn be thrown by extending RuntimeException
+        throw  new throwNormalObject();// can be thrown by extending RuntimeException
     }
 }
