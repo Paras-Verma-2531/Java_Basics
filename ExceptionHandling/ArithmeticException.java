1b4976dcbeef3e030ed9c9788bcba748458940bf
paras <parasverma2531@gmail.com>
2022-11-21 23:48:02 +0530

    Arithmetic Exception

--- ExceptionHandling/ArithmeticException.java
+++ ExceptionHandling/ArithmeticException.java
@@ -1,5 +1,4 @@
 package ExceptionHandling;
-
 public class ArithmeticException {
     public static void main(String[] args) {
         //System.out.println(10/0);// Arithmetic Exception (No try-catch block)
