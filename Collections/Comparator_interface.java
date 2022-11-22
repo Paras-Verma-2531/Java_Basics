818dd2fa413bfa58e6aa21f035f01cbb142a3e4f
paras <parasverma2531@gmail.com>
2022-11-22 17:54:14 +0530

    Comparator

--- Collections/Comparator_interface.java
+++ Collections/Comparator_interface.java
@@ -29,10 +29,8 @@ public class Comparator_interface {
         TreeSet<Integer>ts= new TreeSet<>(new Comparator<Integer>() {
             @Override
             public int compare(Integer o1, Integer o2) {
-                if(o1>o2)
-                    return -1;
-                else if(o1<o2)
-                    return 1;
+                if(o1>o2)return -1;
+                else if(o1<o2)return 1;
                 else
                     return  0;
             }
