97738eb4f3fe06f0188a2681cdf46be8877f78a5
paras <parasverma2531@gmail.com>
2022-12-11 23:08:40 +0530

    Inter-thread-communication

--- Multithreading/CalculateSum.java
+++ Multithreading/CalculateSum.java
@@ -3,8 +3,10 @@ public class CalculateSum {
     private int total=0;
     public synchronized void calSum(int num)throws InterruptedException
     {
-        System.out.println("I a calculating sum");
-        for(int i=1;i<=num;i++)total+=i;
+        System.out.println("I am calculating sum");
+        for(int i=1;i<=num;i++)
+        { Thread.sleep(2000);
+            total+=i;}
         System.out.println("I am going to notify");
         notify();
     }
